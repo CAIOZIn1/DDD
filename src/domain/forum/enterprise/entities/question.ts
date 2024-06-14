1fc641ccd158de597ff4bfc325aa0851e95728f6
CAIOZin1 <sellacaio4@gmail.com>
2024-06-13 23:44:56 -0300

    :test_tube: & :recycle: done: Adicionando novos testes e refatorando outros

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,10 +1,10 @@
 import { Slug } from './value-objects/slug'
-import { Entity } from 'src/core/entities/entity'
-import { UniqueEntityID } from 'src/core/entities/unique-entity-id'
-import { Optional } from 'src/core/types/optional'
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
 import dayjs from 'dayjs'
 
-interface QuestionProps {
+export interface QuestionProps {
   authorId: UniqueEntityID
   bestAnswerId?: UniqueEntityID
   title: string
